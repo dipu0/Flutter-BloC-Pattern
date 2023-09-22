566e1fb2d860575065ae2cd4bc544ed4c301ad06
Md. Asad Chowdhury Dipu <144313141+dipu-polygontech@users.noreply.github.com>
2023-09-22 22:33:42 +0600

    Update home.dart

--- counter/lib/home.dart
+++ counter/lib/home.dart
@@ -1,5 +1,9 @@
+import 'package:counter/bloc/counter_bloc.dart';
+import 'package:counter/bloc/counter_event.dart';
+import 'package:counter/bloc/counter_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -18,7 +22,18 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: _view(context, 0),
+      body:BlocBuilder<CounterBloc, CounterState>(
+        builder: (context, state){
+          if(state is CounterInit){
+            return _view(context, 0);
+          }
+          if(state is CounterUpdate){
+            return _view(context, state.counter);
+          }
+
+          return Container();
+        },
+      ),
     );
   }
 }
@@ -26,21 +41,27 @@ class _MyHomePageState extends State<MyHomePage> {
 Widget _view(BuildContext context, int value) {
   return Center(
     child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           value.toString(),
-          style: TextStyle(fontSize: 26),
+          style: TextStyle(fontSize: 36),
         ),
+        SizedBox(height: 100,),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                context.read<CounterBloc>().add(CounterIncress());
+              },
               child: Icon(Icons.add),
             ),
-            SizedBox(width: 50),
+            SizedBox(width: 100),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                  context.read<CounterBloc>().add(CounterDecress());
+              },
               child: Icon(Icons.minimize),
             ),
           ],
