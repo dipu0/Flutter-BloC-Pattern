f5138af4cbedd0d69fe1032cfad2a92808006ab1
Md. Asad Chowdhury Dipu <144313141+dipu-polygontech@users.noreply.github.com>
2023-09-22 20:32:22 +0600

    Update counter_state.dart

--- counter/lib/bloc/counter_state.dart
+++ counter/lib/bloc/counter_state.dart
@@ -1,6 +1,21 @@
 import 'package:equatable/equatable.dart';
 
-abstract class CounterState extends Equatable{
+abstract class CounterState extends Equatable {}
 
+class CounterInit extends CounterState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
-class CounterInit extends CounterState{}
+
+class CounterUpdate extends CounterState {
+
+final int counter;
+
+CounterUpdate(this.counter);
+
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [counter];
+}
